4a716337f5fbb7dd6ac377e4056f48fc628aacae
NosheenIbra <ibranosheen560@gmail.com>
2023-04-09 22:02:45 +0500

    home page edit

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,9 @@
 <template>
+  <div>
+
     <h1>Hello;user in home page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam facere consectetur nisi rem? Dicta amet temporibus optio mollitia eligendi eaque modi expedita! Adipisci incidunt facere in laboriosam quisquam quia dolorum.</p>
+  </div>
 </template>
 <script>
 export default{
